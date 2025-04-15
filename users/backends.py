0a01f6df1bc7dc543de69c20f5273d4cd99422f6
fgitonga <mfredgitonga@gmail.com>
2025-04-15 14:17:58 +0300

    add custom email sign in auth

---
+++ users/backends.py
@@ -0,0 +1,19 @@
+# users/backends.py (if your app is 'users')
+
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class EmailAuthBackend(ModelBackend):
+    """
+    Authenticate using email instead of username.
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
