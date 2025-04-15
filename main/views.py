24f562001d731a02d920dbd236b88bb631bda3b6
fgitonga <mfredgitonga@gmail.com>
2025-04-15 11:24:26 +0300

    update most requested services in view home page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth import logout as django_logout
-
+from services.models import Service
+from django.db.models import Count
 
 def home(request):
-    return render(request, "main/home.html", {})
+    # Most requested services
+    most_requested = Service.objects.annotate(request_count=Count('requests')).order_by('-request_count')[:5]
+    # Recently created services
+    recent_services = Service.objects.all()[:5]
+    
+    return render(request, "main/home.html", {
+        'most_requested': most_requested,
+        'recent_services': recent_services,
+    })
 
 
 def logout(request):
