d1187358597197adfea5562e2f469400c733156d
fgitonga <mfredgitonga@gmail.com>
2025-04-15 14:20:51 +0300

    update service creatio form

--- services/forms.py
+++ services/forms.py
@@ -1,27 +1,44 @@
 from django import forms
 
-from users.models import Company
-
-
 class CreateNewService(forms.Form):
     name = forms.CharField(max_length=40)
     description = forms.CharField(widget=forms.Textarea, label='Description')
-    price_hour = forms.DecimalField(
-        decimal_places=2, max_digits=5, min_value=0.00)
-    field = forms.ChoiceField(required=True)
+    price_hour = forms.DecimalField(decimal_places=2, max_digits=5, min_value=0.00)
+    field = forms.ChoiceField(required=True, choices=[])
 
-    def __init__(self, *args, choices='', ** kwargs):
-        super(CreateNewService, self).__init__(*args, **kwargs)
-        # adding choices to fields
+    def __init__(self, *args, choices=None, **kwargs):
+        super().__init__(*args, **kwargs)
         if choices:
-            self.fields['field'].choices = choices
-        # adding placeholders to form fields
-        self.fields['name'].widget.attrs['placeholder'] = 'Enter Service Name'
-        self.fields['description'].widget.attrs['placeholder'] = 'Enter Description'
-        self.fields['price_hour'].widget.attrs['placeholder'] = 'Enter Price per Hour'
+            self.fields['field'].choices = choices if choices else [('', 'Select a field')]
 
-        self.fields['name'].widget.attrs['autocomplete'] = 'off'
+        # adding placeholders to form fields
+        self.fields['name'].widget.attrs.update({
+            'placeholder': 'Enter Service Name',
+            'autocomplete': 'off',
+            'class': 'form-control',
+        })
+
+        self.fields['description'].widget.attrs.update({
+            'placeholder': 'Enter Description',
+            'class': 'form-control',
+        })
+
+        self.fields['price_hour'].widget.attrs.update({
+            'placeholder': 'Enter Price per Hour',
+            'class': 'form-control',
+        })
+
+        self.fields['field'].widget.attrs.update({
+            'class': 'form-control',
+        })
 
 
 class RequestServiceForm(forms.Form):
-    pass
+    address = forms.CharField(
+        max_length=255, 
+        widget=forms.TextInput(attrs={'placeholder': 'Enter service address'})
+    )
+    service_time = forms.IntegerField(
+        min_value=1,
+        widget=forms.NumberInput(attrs={'placeholder': 'Hours needed'})
+    )
