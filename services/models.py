f75d9b183a69c3c7edc7500816b39ad5619f5249
fgitonga <mfredgitonga@gmail.com>
2025-04-15 14:21:13 +0300

    add service request form

--- services/models.py
+++ services/models.py
@@ -1,16 +1,14 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from users.models import Company, Customer
-
+from users.models import Company, Customer, User
+from django.utils import timezone
 
 class Service(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
     description = models.TextField()
-    price_hour = models.DecimalField(decimal_places=2, max_digits=100)
+    price_hour = models.DecimalField(decimal_places=2, max_digits=8)
     rating = models.IntegerField(validators=[MinValueValidator(
         0), MaxValueValidator(5)], default=0)
     choices = (
@@ -32,3 +30,16 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+
+class ServiceRequest(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='requests')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='requested_services')
+    address = models.CharField(max_length=255)
+    service_time = models.PositiveIntegerField(help_text="Service time in hours")
+    date_requested = models.DateTimeField(default=timezone.now)
+    
+    def get_total_cost(self):
+        return self.service.price_per_hour * self.service_time
+    
+    def __str__(self):
+        return f"{self.customer.user.username} - {self.service.name}"
